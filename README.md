117b2ac30f56b18d0af0879f1c8addf0cf8482c6
Mike Ohlsen <online@mohlsen.com>
2015-03-05 22:52:56 -0500

    working socket.io

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # socketIOPlayground
 a tiny little socket.IO test
+
+## To Run:
+
+1. Clone repo
+2. Run ```npm install```
+3. Run ```node index.js```
+4. Open a browser to [http://localhost:3000/](http://localhost:3000/)
+
--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 //http://expressjs.com/starter/hello-world.html
 //http://expressjs.com/starter/static-files.html
 
+
 var express = require('express')
-var app = express()
-//var io = require('socket.io')(http);
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static('www'));
 app.use('/lib', express.static('node_modules'));
 
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-// });
+io.on('connection', function(socket){
+  console.log('a user connected');
 
-var server = app.listen(3000, function () {
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+var server = http.listen(3000, function(){
 
   var host = server.address().address
   var port = server.address().port
 
   console.log('Example app listening at http://%s:%s', host, port)
 
-})
+});
+
