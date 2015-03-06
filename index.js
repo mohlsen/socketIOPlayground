3918aff6820eed9d6aa1feb0233149554a1dd427
Mike Ohlsen <online@mohlsen.com>
2015-03-05 23:38:10 -0500

    working socket.io emitting data, client side console log only

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+//Examples from:
 //http://expressjs.com/starter/hello-world.html
 //http://expressjs.com/starter/static-files.html
-
+//http://socket.io/get-started/chat/
 
 var express = require('express')
 var app = express();
@@ -11,11 +12,33 @@ app.use(express.static('www'));
 app.use('/lib', express.static('node_modules'));
 
 io.on('connection', function(socket){
+
+  var iterations = 0;
+
   console.log('a user connected');
 
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
+
+  var cancelInterval = setInterval(function() { 
+  	iterations++;
+  	io.emit('deviceDetails', { 
+         "address": "10.0.0." + Math.floor(Math.random() * 999),
+         "port": 8080,
+         "deviceType": 1,
+         "productCode": 123,
+         "serialNumber": "QWERTY" + Math.floor(Math.random() * 10000),
+         "productName": "Device " + iterations,
+       }
+  	);
+
+  	if (iterations >= 5) {
+        clearInterval(cancelInterval);
+        console.log('done sending at interval');
+    }
+  }, 500);
+
 });
 
 var server = http.listen(3000, function(){
@@ -26,4 +49,7 @@ var server = http.listen(3000, function(){
   console.log('Example app listening at http://%s:%s', host, port)
 
 });
+
+
+
 
